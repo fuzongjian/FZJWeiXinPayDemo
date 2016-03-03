b80bc30cb3cfd3d0d5a2f26bdb70d038928a2757
fuzongjian <fdkj0002@fdkjCSRdeMac-mini.local>
2016-03-03 15:25:48 +0800

    1.1
    手动生成微信支付订单，只要有相关参数的配置，便可以便可以完成支付

--- FZJWeiXinPayDemo/FZJWeiXinPayDemo/AppDelegate.m
+++ FZJWeiXinPayDemo/FZJWeiXinPayDemo/AppDelegate.m
@@ -8,7 +8,8 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+
+@interface AppDelegate ()<WXApiDelegate>
 
 @end
 
@@ -16,10 +17,71 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
+    [WXApi registerApp:APP_KEY withDescription:@"微信测试"];//记得配置白名单
+    
     return YES;
 }
+-(BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(nonnull id)annotation{
+    if ([url.host isEqualToString:@"pay"]) {
+        return [WXApi handleOpenURL:url delegate:self];
+    }
+    return YES;
+    
+}
+-(BOOL)application:(UIApplication *)app handleOpenURL:(nonnull NSURL *)url{
+    if ([url.host isEqualToString:@"pay"]) {
+        return [WXApi handleOpenURL:url delegate:self];
+    }
+    return YES;
+}
+-(void)onResp:(BaseResp *)resp{
+    NSString *strTitle;
+    if ([resp isKindOfClass:[SendMessageToWXResp class]]) {
+        strTitle = [NSString stringWithFormat:@"发送媒体消息结果"];
+    }
+    if ([resp isKindOfClass:[PayResp class]]) {
+        strTitle = [NSString stringWithFormat:@"支付结果"];
+        switch (resp.errCode) {
+            case WXSuccess:
+            {
+                NSLog(@"支付结果: 成功!");
+            }
+                break;
+            case WXErrCodeCommon:
+            { //签名错误、未注册APPID、项目设置APPID不正确、注册的APPID与设置的不匹配、其他异常等
+                
+                NSLog(@"支付结果: 失败!");
+            }
+                break;
+            case WXErrCodeUserCancel:
+            { //用户点击取消并返回
+                NSLog(@"用户点击取消并返回");
+            }
+                break;
+            case WXErrCodeSentFail:
+            { //发送失败
+                NSLog(@"发送失败");
+            }
+                break;
+            case WXErrCodeUnsupport:
+            { //微信不支持
+                NSLog(@"微信不支持");
+            }
+                break;
+            case WXErrCodeAuthDeny:
+            { //授权失败
+                NSLog(@"授权失败");
+            }
+                break;
+            default:
+                break;
+        }
+        //------------------------
+    }
 
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
